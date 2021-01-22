f599549a96d05b3e16d1d2b20e8bace068242c60
Nouha <nouhagdira@gmail.com>
2021-01-22 17:25:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',//the template defines the view of this component
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ //importe modules that this app is dependent on
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//specify the services that this module will make use of
+  bootstrap: [AppComponent]// AppComponent is the root component of our app
 })
 export class AppModule { }
