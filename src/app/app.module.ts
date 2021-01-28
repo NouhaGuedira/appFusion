06fafbeba0c68fe8f656bd022b3b80b994e38a82
Nouha <nouhagdira@gmail.com>
2021-01-28 11:18:07 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import  {MatListModule} from '@angular/material/list';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon'
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [ //importe modules that this app is dependent on
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],//specify the services that this module will make use of
   bootstrap: [AppComponent]// AppComponent is the root component of our app
