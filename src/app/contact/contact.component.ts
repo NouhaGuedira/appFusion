a746da4424288ebe9a0c2b41d99f69692ad9166d
Nouha <nouhagdira@gmail.com>
2021-02-05 17:41:08 +0100

    RxJS Forms Part3 /Validation in code side

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -25,18 +25,82 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //adding formErrors array for validation of fields 
+  //in order to map each field name with the message error 
+  formErrors : { [k: string]: string | undefined} = { //index signature explicitly declared
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages : { [fieldName:string] : { [k: string]: string | undefined}  } = { //index signature explicitly declared
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
   createForm(){
     this.feedbackForm =this.fb.group({ //our data structure 
       
-      firstname: ['',Validators.required],
-      lastname: ['',Validators.required],
-      telnum: ['' ,Validators.required],
-      email: ['',Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
-
     });
+    this.onChanges();
+
+  }
+
+  onChanges(): void{
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    //reset validation messages now
+
+  }
+
+  onValueChanged(data?: any){
+    if(this.feedbackForm){
+        const form = this.feedbackForm;
+        //control each field set by the user input
+        //check wether its invalid 
+        //if invalid we get the specific message for this case 
+        //and map it with the surrent field name in our formError object
+        for(const field in this.formErrors){
+          if(this.formErrors.hasOwnProperty(field)) { //
+             // clear previous error message (if any)
+             this.formErrors[field] = '';
+             const control = form.get(field);
+             if(control && control.dirty && control.invalid){
+               const messages = this.validationMessages[field]; //we map the messages to our current field name object of errorsMessages
+
+               for (const key in control.errors) {
+                  if (control.errors.hasOwnProperty(key)) {
+                    this.formErrors[field] += messages[key] + ' ';
+                  }
+                }
+              }
+          }
+        }
+    }
+    return;
   }
   onSubmit(){
     this.feedback = this.feedbackForm.value;
