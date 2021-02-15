71fddce1234615ae97d501dfc1da6c638bd03ff1
Nouha <nouhagdira@gmail.com>
2021-02-15 23:36:57 +0100

    Unit test part1

--- src/app/spec/menu.component.spec.ts
+++ src/app/spec/menu.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { MenuComponent } from './menu.component';
+import { MenuComponent } from '../menu/menu.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { Dish } from '../shared/dish';
